b5ffeb6afbe52e1768122021fd565fa78a1f80ad
Kevin Lam <klam@lcs.on.ca>
2025-03-03 14:43:17 -0500

    Reorganised the app files to fit the criteria set

---
+++ FuelEff/Model/Efficiency.swift
@@ -0,0 +1,7 @@
+//
+//  Efficiency.swift
+//  FuelEff
+//
+//  Created by Kevin Lam on 3/3/2025.
+//
+
---
+++ FuelEff/ViewModel/EfficiencyModel.swift
@@ -0,0 +1,7 @@
+//
+//  EfficiencyModel.swift
+//  FuelEff
+//
+//  Created by Kevin Lam on 3/3/2025.
+//
+
