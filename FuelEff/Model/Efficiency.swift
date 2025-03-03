a54feb126863dfcca0e0385b76c191580ff35b6a
Kevin Lam <klam@lcs.on.ca>
2025-03-03 14:44:07 -0500

    Added the model into the app

--- FuelEff/Model/Efficiency.swift
+++ FuelEff/Model/Efficiency.swift
@@ -5,3 +5,19 @@
 //  Created by Kevin Lam on 3/3/2025.
 //
 
+import Foundation
+
+// MODEL
+struct FuelEfficiency {
+    
+    // MARK: Stored properties
+    
+    var distance: Double
+    var fuelUsed: Double
+    
+    // MARK: Computed properties
+    
+    var efficiency: Double {
+        return (fuelUsed / distance) * 100
+    }
+}
