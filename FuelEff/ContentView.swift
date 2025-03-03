488bba64626222af61c01d65ca3a5472adfe6022
Kevin Lam <klam@lcs.on.ca>
2025-03-03 14:31:30 -0500

    Initial Commit

---
+++ FuelEff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FuelEff
+//
+//  Created by Kevin Lam on 3/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FuelEff/FuelEffApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelEffApp.swift
+//  FuelEff
+//
+//  Created by Kevin Lam on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct FuelEffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
